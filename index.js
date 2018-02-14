7aa848228b7921e94c9cbab19362e9a86606e58c
Mark Cruz <markchriscruz@yahoo.com>
2018-02-14 07:20:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,6 +74,6 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber == 'undefined') {
-    console.log("Sorry, we don't have a credit card on file for you.")
+    console.log("Sorry, we don\'t have a credit card on file for you.")
   }
 }
